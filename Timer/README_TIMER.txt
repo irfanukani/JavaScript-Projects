245cd1e1299a2b1d55b38e38b3683b90c806e8d8
irfanukani <irfanukani2@gmail.com>
2021-01-09 18:56:28 +0530

    Added Timer

---
+++ Timer/README_TIMER.txt
@@ -0,0 +1,9 @@
+Asset Links :
+
+Image : https://images.unsplash.com/photo-1564303449-b4a1a1ba2037?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80
+
+Fonts : https://fonts.googleapis.com/css2?family=Quicksand&display=swap
+
+Design : https://uidesigndaily.com/posts/sketch-countdown-timer-day-312
+
+Icons : https://www.flaticon.com/
---
+++ Timer/src/timer.js
@@ -0,0 +1,37 @@
+var DAY = document.getElementById("days");
+var HOURS = document.getElementById("hours");
+var MINUTES = document.getElementById("Minutes");
+var SECONDS = document.getElementById("Seconds");
+var Theme = document.getElementById("theme");
+
+setInterval(Timer, 1000);
+
+function Timer() {
+  var Today = new Date().getTime();
+  var Fest = new Date("Sep 19, 2021").getTime();
+
+  var Time = Math.abs(Fest - Today);
+
+  var days = Math.floor(Time / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((Time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((Time % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((Time % (1000 * 60)) / 1000);
+
+  if (days < 9) {
+    days = "0" + days;
+  }
+  if (hours < 9) {
+    hours = "0" + hours;
+  }
+  if (minutes < 9) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 9) {
+    seconds = "0" + seconds;
+  }
+
+  DAY.innerHTML = days;
+  HOURS.innerHTML = hours;
+  MINUTES.innerHTML = minutes;
+  SECONDS.innerHTML = seconds;
+}
