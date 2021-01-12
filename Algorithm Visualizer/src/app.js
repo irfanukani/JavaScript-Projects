b1e11ccf4e2017945a18de5a6b10a6942979fae0
irfanukani <irfanukani2@gmail.com>
2021-01-12 20:30:45 +0530

    Added Bubble Sort Functionallity

--- Algorithm Visualizer/src/app.js
+++ Algorithm Visualizer/src/app.js
@@ -4,9 +4,78 @@ UI Class :
     -Get data from form
     -make array of data
 */
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function DisplayBars(obj) {
+  var app = document.getElementById("app");
+
+  app.innerHTML = "";
+
+  // console.log(obj.Array);
+
+  let i;
+  for (i = 0; i < obj.Array.length; i++) {
+    let box = document.createElement("div");
+    box.innerHTML = obj.Array[i];
+    box.style.margin = "10px";
+    box.classList.add("p-4");
+    box.classList.add("border");
+    box.classList.add("box");
+
+    if (i == obj.e1) {
+      box.classList.add("border-info");
+      setTimeout(() => {
+        box.classList.add("border-danger");
+        box.classList.remove("border-info");
+        box.innerHTML = obj.Array[obj.e2];
+      }, 1000);
+    }
+    if (i == obj.e2) {
+      box.classList.add("border-danger");
+      setTimeout(() => {
+        box.classList.add("border-info");
+        box.classList.remove("border-danger");
+        box.innerHTML = obj.Array[obj.e1];
+      }, 1000);
+    }
+    app.appendChild(box);
+  }
+}
 
 class UI {
-  static DisplayAlgorithm() {}
+  static DisplayAlgorithm(obj) {
+    const Array = obj.Array;
+
+    DisplayBars({
+      Array: Array,
+      e1: 1000,
+      e2: 1000,
+    });
+
+    async function bubbleSort(inputArr) {
+      let len = inputArr.length;
+      for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+          if (inputArr[j] > inputArr[j + 1]) {
+            await DisplayBars({
+              Array: inputArr,
+              e1: j,
+              e2: j + 1,
+            });
+            await sleep(3000);
+            let tmp = inputArr[j];
+            inputArr[j] = inputArr[j + 1];
+            inputArr[j + 1] = tmp;
+          }
+        }
+      }
+      alert("Array sorted :)");
+      return inputArr;
+    }
+    setTimeout(bubbleSort(Array), 2000);
+  }
 
   static getData() {
     var Selection = document.getElementById("algo");
@@ -36,8 +105,7 @@ class UI {
 
       _data.Array = Temp;
     }
-
-    console.log(_data);
+    // console.log(_data);
     return _data;
   }
 
